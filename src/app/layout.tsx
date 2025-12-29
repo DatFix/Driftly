90e45def796a77e776788802ef8bcbad9a82f7c0
DatFix <vinhdat998@gmail.com>
2025-12-29 11:20:15 +0700

    update metadata & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,22 @@ import { ThemeProvider } from "next-themes";
 
 import RootContent from "./RootContent";
 import { InitAdmin } from "@/api/auth/admin.api";
+import { Metadata } from "next";
 
 const montserrat = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-montserrat",
 });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Driftly",
+    template: "%s | Driftly",
+  },
+  description: "Social platform by Driftly",
+  applicationName: "Driftly",
+};
 
 export default async function RootLayout({
   children,
